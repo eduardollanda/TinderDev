61d3ed93f429525028aeb78a2c5cea83176683b1
eduardollanda <eduardohollanda14@gmail.com>
2019-08-31 01:42:29 -0300

    atualizando antes de dormir kkkk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import Login from './pages/Login.js'
+import Routes from './routes.js'
 function App() {
   return (
-    <Login/>
+    <Routes/>
   );
 }
 
